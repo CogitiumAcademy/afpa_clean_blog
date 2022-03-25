2eb0933cbdd44a19f9d32000e7496a34bd40ed40
Cogitium <info@cogitium.com>
2022-03-25 10:41:05 +0100

    Intégration de requetes préparées

--- model/contact.add.model.php
+++ model/contact.add.model.php
@@ -4,14 +4,35 @@ include("config/config.inc.php");
 include("model/pdo.inc.php");
 
 try {
+    /* Version sans requete préparée
+
     //var_dump($_POST);
+    /*
     $query = "INSERT INTO blog_contacts
-        (contact_name2, contact_email, contact_phone, contact_message)
+        (contact_name, contact_email, contact_phone, contact_message)
         VALUES
         ('" . addslashes($_POST["name"]) . "', '" . $_POST["email"] . "', '" . $_POST["phone"] . "', '" . addslashes($_POST["message"]) ."')";
-
-    //die($query);
     $req = $pdo->exec($query);
+    */
+
+
+    // Version requete préparée
+
+    $query = "INSERT INTO blog_contacts
+        (contact_name, contact_email, contact_phone, contact_message)
+        VALUES
+        (:contact_name, :contact_email, :contact_phone, :contact_message)";
+    //die($query);
+    
+    $curseur = $pdo->prepare($query);
+
+    $curseur->bindValue(':contact_name', $_POST["name"], PDO::PARAM_STR);
+    $curseur->bindValue(':contact_email', $_POST["email"], PDO::PARAM_STR);
+    $curseur->bindValue(':contact_phone', $_POST["phone"], PDO::PARAM_STR);
+    $curseur->bindValue(':contact_message', $_POST["message"], PDO::PARAM_STR);
+
+    $curseur->execute();
+
 } catch (Exception $e) {
     //die("Erreur MySQL : " . $e->getMessage());
     header("Location: index.php?erreur=1");
--- model/post.model.php
+++ model/post.model.php
@@ -6,24 +6,52 @@ include("config/config.inc.php");
 include("model/pdo.inc.php");
 
 try {
-    $query = "
-    SELECT post_date, post_content, post_title, post_img_url, display_name, cat_descr 
-    FROM blog_posts
-    
-    INNER JOIN blog_users
-    ON post_author = ID
-    
-    INNER JOIN blog_categories
-    ON post_category = cat_id
-    
-    WHERE post_ID = " . $_GET["article"];
+    /* Version sans requete préparée
 
-    //die($query);
+    $query = "SELECT post_date, post_content, post_title, post_img_url, display_name, cat_descr 
+                FROM blog_posts
+                
+                INNER JOIN blog_users
+                ON post_author = ID
+                
+                INNER JOIN blog_categories
+                ON post_category = cat_id
+                
+                WHERE post_ID = " . $_GET["article"];
+
+    die($query);
 
     $req = $pdo->query($query);
 
     $data = $req->fetch();
     //var_dump($data); 
+    */
+
+    // Version requete préparée
+
+    $query = "SELECT post_date, post_content, post_title, post_img_url, display_name, cat_descr 
+                FROM blog_posts
+                
+                INNER JOIN blog_users
+                ON post_author = ID
+                
+                INNER JOIN blog_categories
+                ON post_category = cat_id
+                
+                WHERE post_ID = :article";
+
+    $curseur = $pdo->prepare($query);
+
+    $curseur->bindValue(':article', $_GET["article"], PDO::PARAM_INT);
+
+    $curseur->execute();
+
+    $curseur->setFetchMode(PDO::FETCH_ASSOC);
+    $data = $curseur->fetch();
+
+    var_dump($data);
+    exit;
+
 
 } catch(Exception $e) {
     die("Erreur MySQL : " . $e->getMessage());
